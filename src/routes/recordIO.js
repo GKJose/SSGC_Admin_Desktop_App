4ec5d2f341d00a022ca153895ff9676121864337
Jose <mendezj8121@uhcl.edu>
2022-04-15 17:57:19 -0500

    made recordIO a class component, using states to update

--- src/routes/recordIO.js
+++ src/routes/recordIO.js
@@ -1,3 +1,4 @@
+import { render } from "@testing-library/react";
 import React,{ useState } from "react";
 const electron= window.require("electron");
 const ipcRenderer = electron.ipcRenderer;
@@ -5,49 +6,50 @@ const ipcRenderer = electron.ipcRenderer;
 const columnNames = ["Log Type","Client IP","Client Name","Data"];
 const clientLogs = [];
 
-function useForceUpdate(){
-  const [value,setValue] = useState(0);
-  return () => setValue((value) => value + 1);
+class RecordIO extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {count:0};
 }
+  componentDidMount(){
+    let table = document.createElement("table");
+    let trow = table.insertRow(-1);
 
-export default function RecordIO(props) {
-  let table = document.createElement("table");
-  let trow = table.insertRow(-1);
-  for(var i = 0; i < columnNames.length; i++){
-    let theader = document.createElement("th");
-    theader.innerHTML =  columnNames[i];
-    trow.appendChild(theader);
-  }
+    for(var i = 0; i < columnNames.length; i++){
+      let theader = document.createElement("th");
+      theader.innerHTML =  columnNames[i];
+      trow.appendChild(theader);
+    }
+    
+    for(i = 0; i < clientLogs.length; i++){
+      var clientLog = clientLogs[i];
+      console.log(clientLog);
+      trow = table.insertRow(-1);
+      let cell = trow.insertCell(-1);
+      cell.innerHTML = clientLog.ssgcType;
+      cell = trow.insertCell(-1);
+      cell.innerHTML = clientLog.clientIP;
+      cell = trow.insertCell(-1);
+      cell.innerHTML = clientLog.clientName;
+      cell = trow.insertCell(-1);
+      cell.innerHTML = clientLog.data;
+    }
 
-  ipcRenderer.on("clientLog",(event,data) => {
-    clientLogs.push(data);
-  });
-  
-  for(i = 0; i < clientLogs.length; i++){
-    var clientLog = clientLogs[i];
-    console.log(clientLog);
-    trow = table.insertRow(-1);
-    let cell = trow.insertCell(-1);
-    cell.innerHTML = clientLog.ssgcType;
-    cell = trow.insertCell(-1);
-    cell.innerHTML = clientLog.clientIP;
-    cell = trow.insertCell(-1);
-    cell.innerHTML = clientLog.clientName;
-    cell = trow.insertCell(-1);
-    cell.innerHTML = clientLog.data;
-
-  }
-  React.useEffect(()=>{
     let tableElement = document.getElementById("table");
     tableElement.innerHTML = "";
     tableElement.appendChild(table);
-  });
-
+    ipcRenderer.on("clientLog",(event,data) => {
+      clientLogs.push(data);
+      this.setState(count => count + 1);
+    });
+  }
+  render(){
     return (
-        <div className='recordIO' data-theme ={props.theme}>
+        <div className='recordIO' data-theme ={this.props.theme}>
           <h2>Record I/O</h2>
-          <button type="button" onClick={useForceUpdate}>Refresh page</button>
           <table id="table" align = "center" border="1px"></table>
         </div>
       );
+  }
 }
+export default RecordIO;
