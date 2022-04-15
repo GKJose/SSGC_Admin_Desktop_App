fc31f7b1187fe6b791eccb7cd097b0ab84036c2f
Jose <mendezj8121@uhcl.edu>
2022-04-15 18:05:41 -0500

    refactored, removed keys

--- src/index.js
+++ src/index.js
@@ -14,51 +14,28 @@ import RestrictPermissions from './routes/restrictPermissions'
 import reportWebVitals from './reportWebVitals';
 const electron = window.require('electron');
 const ipcRenderer = electron.ipcRenderer;
-let theme = "light";
-let lightPaths = [
-  <Route path = "/" element = {<App key = "App" theme = "light"/>}/>,
-  <Route path = "/routes/aboutUs" element = {<AboutUs key = "AboutUs" theme = "light" />}/>,
-  <Route path = "/routes/checkForUpdates" element = {<CheckForUpdates key = "CheckForUpdates" theme = "light" />}/>,
-  <Route path = "/routes/gettingStarted" element = {<GettingStarted key = "GettingStarted" theme = "light"/>}/>,
-  <Route path = "/routes/recordIO" element = {<RecordIO key = "RecordIO" theme = "light"/>}/>,
-  <Route path = "/routes/remoteViewCalculators" element = {<RemoteViewCalculators key = "RemoteViewCalculators" theme = "light"/>}/>,
-  <Route path = "/routes/restrictPermissions" element = {<RestrictPermissions key = "RestrictPermissions"  theme = "light"/>}/>    
-]
-let darkPaths = [
-  <Route path = "/" element = {<App key = "App" theme = "dark"/>}/>,
-  <Route path = "/routes/aboutUs" element = {<AboutUs key = "AboutUs" theme = "dark" />}/>,
-  <Route path = "/routes/checkForUpdates" element = {<CheckForUpdates key = "CheckForUpdates" theme = "dark" />}/>,
-  <Route path = "/routes/gettingStarted" element = {<GettingStarted key = "GettingStarted" theme = "dark"/>}/>,
-  <Route path = "/routes/recordIO" element = {<RecordIO key = "RecordIO" theme = "dark"/>}/>,
-  <Route path = "/routes/remoteViewCalculators" element = {<RemoteViewCalculators key = "RemoteViewCalculators" theme = "dark"/>}/>,
-  <Route path = "/routes/restrictPermissions" element = {<RestrictPermissions key = "RestrictPermissions" theme = "dark"/>}/>    
-]
+
+
 ipcRenderer.on('change-theme',(event,data) =>{
-  theme = data;
-  renderPaths();
+  renderPaths(data);
 });
-function renderPaths(){
-  if(theme === "light"){
+function renderPaths(theme){
     ReactDOM.render(
       <BrowserRouter>
       <Routes>
-        {lightPaths} 
+        <Route path = "/" element = {<App theme = {theme}/>} />,
+        <Route path = "/routes/aboutUs" element = {<AboutUs theme = {theme} />}/>,
+        <Route path = "/routes/checkForUpdates" element = {<CheckForUpdates theme = {theme} />}/>,
+        <Route path = "/routes/gettingStarted" element = {<GettingStarted  theme = {theme}/>}/>,
+        <Route path = "/routes/recordIO" element = {<RecordIO  theme = {theme}/>}/>,
+        <Route path = "/routes/remoteViewCalculators" element = {<RemoteViewCalculators key = "RemoteViewCalculators" theme = {theme}/>}/>,
+        <Route path = "/routes/restrictPermissions" element = {<RestrictPermissions  theme = {theme}/>}/>    
       </Routes>
     </BrowserRouter>,
       document.getElementById('root')
     );
-  }else{
-    ReactDOM.render(
-      <BrowserRouter>
-      <Routes>
-        {darkPaths} 
-      </Routes>
-    </BrowserRouter>,
-      document.getElementById('root')
-    );
-  }
 }
-renderPaths();
+renderPaths("light");
 
 
 // If you want to start measuring performance in your app, pass a function
